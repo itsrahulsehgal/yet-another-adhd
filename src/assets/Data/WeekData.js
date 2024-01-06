e4afd7579f6f43b9cfb50d40488d10e02c6fbb21
itsrahulsehgal <rahul.sehgal049@gmail.com>
2024-01-06 23:06:27 +0530

    assets added

---
+++ src/assets/Data/WeekData.js
@@ -0,0 +1,37 @@
+export const WeeklyData = [
+  {
+    month: 'Mon',
+    visitors: 5,
+    sales: 2,
+  },
+  {
+    month: 'Tue',
+    visitors: 7,
+    sales: 30,
+  },
+  {
+    month: 'Wed',
+    visitors: 9,
+    sales: 20,
+  },
+  {
+    month: 'Thu',
+    visitors: 11,
+    sales: 10,
+  },
+  {
+    month: 'Fri',
+    visitors: 10,
+    sales: 24,
+  },
+  {
+    month: 'Sat',
+    visitors: 13,
+    sales: 29,
+  },
+  {
+    month: 'Sun',
+    visitors: 12,
+    sales: 6,
+  },
+];
---
+++ src/assets/Data/YearData.js
@@ -0,0 +1,62 @@
+export const YearlyData = [
+  {
+    month: 'Jan',
+    visitors: 5,
+    sales: 20,
+  },
+  {
+    month: 'Feb',
+    visitors: 7,
+    sales: 26,
+  },
+  {
+    month: 'Mar',
+    visitors: 9,
+    sales: 20,
+  },
+  {
+    month: 'Apr',
+    visitors: 11,
+    sales: 29,
+  },
+  {
+    month: 'May',
+    visitors: 10,
+    sales: 30,
+  },
+  {
+    month: 'June',
+    visitors: 13,
+    sales: 21,
+  },
+  {
+    month: 'July',
+    visitors: 12,
+    sales: 19,
+  },
+  {
+    month: 'Aug',
+    visitors: 11,
+    sales: 13,
+  },
+  {
+    month: 'Sep',
+    visitors: 9,
+    sales:23,
+  },
+  {
+    month: 'Oct',
+    visitors: 10,
+    sales: 19,
+  },
+  {
+    month: 'Nov',
+    visitors: 11,
+    sales: 9,
+  },
+  {
+    month: 'Dec',
+    visitors: 10,
+    sales: 20,
+  },
+];
